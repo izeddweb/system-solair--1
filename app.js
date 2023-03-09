9318d87268856124a9abc7d1e93342b77621b8e9
izeddweb <abzedd61@gmail.com>
2023-03-09 15:59:12 +0100

    changing stratigy

---
+++ app.js
@@ -0,0 +1,28 @@
+// set Variables
+const planetSun = document.querySelector('.sun');
+const modal = document.querySelector('.modal');
+const anim = document.querySelector('.anim');
+console.log(anim);
+// set function
+
+planetSun.onclick = function () {
+  modal.classList.add('show');
+  anim.classList.add('translate');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
